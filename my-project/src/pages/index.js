c6188423741d038e1eef1fac43ed83e053f7d419
nanak <sidhu.nds22@gmail.com>
2023-02-11 22:58:08 +0530

    added post functionality for comments

--- my-project/src/pages/index.js
+++ my-project/src/pages/index.js
@@ -4,26 +4,42 @@ import { useEffect, useState } from "react";
 
 
 export default function Home() {
-
+    const [content,setContent]=useState();
     const [postData, setPostData] = useState([]);
     const getAllPosts = async () => {
-        const resultList = await pb.collection('expert_post').getList(1, 50, {sort:"-created", '$autoCancel': false,expand: 'author'
+        const resultList = await pb.collection('expert_post').getList(1, 50, {sort:"-created", '$autoCancel': false,expand: 'author,comment'
         });
         setPostData(resultList.items)
     }
 
-    console.log(postData)
-
     useEffect(()=>{
         getAllPosts();
-    }, [])
+        console.log(postData)
+    },[content])
+
+    async function create_comment(id,index){
+        const data = {
+            "author": pb.authStore.model.id,
+            "content": content
+        };
+        const record = await pb.collection('comment').create(data);
+        console.log(record)
+        console.log(id)
+        console.log(index)
+        const comment = {
+            "comment": [
+                ...postData[index].comment,record.id,
+            ]
+        };
+        const record2 = await pb.collection('expert_post').update(id, comment);
+    }
 
     return (
         <>
             <Sidebar />
             <div className="p-4 sm:ml-64 max-w-4xl  h-full">
 
-                {postData.map((post)=>{
+                {postData.map((post,index)=>{
                     return (
                         <>
                         <div class="w-full p-6 h-auto bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mt-4">
@@ -47,8 +63,9 @@ export default function Home() {
                     <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-black rounded-lg  focus:ring-4 focus:outline-none focus:ring-blue-300  dark:focus:ring-blue-800 hover:text-green-400">
                     <p className="mx-3"><ion-icon  name="chatbox-ellipses-outline"></ion-icon></p>
                         Comments
-                        
                     </a>
+                    <input type='text' name='content' placeholder="Add comment" value={content} onChange={(e)=>{setContent(e.target.value)}} ></input>
+                    <button onClick={()=>{create_comment(post.id,index)}}>Click Me</button>
                 </div>
                         </>
                     )
