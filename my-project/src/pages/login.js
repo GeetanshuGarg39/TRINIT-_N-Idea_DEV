b8116d5e2ff9eb15b3f70d0ba9a1380223cb444e
Geetanshu Garg <geetanshugarg39@gmail.com>
2023-02-11 14:37:06 +0530

    Minor changes in CSS of login and signup pages

--- my-project/src/pages/login.js
+++ my-project/src/pages/login.js
@@ -31,12 +31,12 @@ const login = () => {
 
   return (
     <>
-    <div className = "flex flex-col justify-center items-center h-screen bg-green-300">
+    <div className = "flex flex-col justify-center items-center h-screen bg-slate-50">
         
     <form className = "flex flex-col w-1/4 p-8 items-center rounded-2xl justify-center drop-shadow-2xl bg-white" onSubmit= {handleSubmit(log_in)}>
-      <h1 className="text-6xl bold mb-8 "> Login </h1>
-      <input className = "p-2 m-3 w-full h-1/3 bg-slate-200 rounded-xl " required type="text" placeholder="Username" name = "username" {...register('username')}></input>
-      <input className = "p-2 m-3 w-full h-1/3 bg-slate-200 rounded-xl " required type = "password" placeholder="Password" name = "password" {...register('password')}></input>
+      <h1 className="text-4xl bold mb-6 "> Login </h1>
+      <input className = "p-2 m-3 w-full h-1/3 bg-slate-100 rounded-xl " required type="text" placeholder="Username" name = "username" {...register('username')}></input>
+      <input className = "p-2 m-3 w-full h-1/3 bg-slate-100 rounded-xl " required type = "password" placeholder="Password" name = "password" {...register('password')}></input>
       <button className=" bg-emerald-600 text-center text-white p-2 mt-4 rounded-full h-1/3  w-full active:bg-green-400" type = "Submit" disabled={isLoading}>{isLoading? "Loading":"Login"}</button>
     </form>
     <div className="flex flex-col w-1/4 p-6 mt-3 items-center rounded-2xl justify-center drop-shadow-2xl bg-white">
--- my-project/src/pages/signup.js
+++ my-project/src/pages/signup.js
@@ -20,15 +20,15 @@ const signup = () => {
   } 
   return (
     <>
-    <div className = "flex flex-col justify-center items-center h-screen bg-green-300">
+    <div className = " flex flex-col justify-center items-center h-screen bg-slate-50">
         
-    <form className = "flex flex-col w-1/4 p-11 items-center rounded-2xl justify-center drop-shadow-2xl bg-white" onSubmit={handleSubmit(sign_up)}>
-      <h1 className="text-6xl bold mb-8 font-medium "> Sign Up </h1>
-      <input className = "p-2 m-3 w-full h-1/3 bg-slate-200 rounded-xl " required type="text" placeholder="Username" name = "username"  {...register('username')}></input>
-      <input className = "p-2 m-3 w-full h-1/3 bg-slate-200 rounded-xl " required type="email" placeholder="Email" name = "email" {...register('email')} ></input>
-      <input className = "p-2 m-3 w-full h-1/3 bg-slate-200 rounded-xl " required type = "password" placeholder="Password" name = "password" {...register('password')}></input>
-      <input className = "p-2 m-3 w-full h-1/3 bg-slate-200 rounded-xl " required type = "password" placeholder="Confirm Password" name = "passwordConfirm" {...register('passwordConfirm')}></input>
-      <input className = "p-2 m-3 w-full h-1/3 bg-slate-200 rounded-xl " required type = "text" placeholder="Farmer/Expert" name = "group" {...register('group')}></input>
+    <form className = "flex flex-col w-1/4 p-8 items-center rounded-2xl justify-center drop-shadow-xl bg-white" onSubmit={handleSubmit(sign_up)}>
+      <h1 className="text-5xl bold mb-6 font-medium "> Sign Up </h1>
+      <input className = "p-2 m-3 w-full h-1/3 bg-slate-100 rounded-xl " required type="text" placeholder="Username" name = "username"  {...register('username')}></input>
+      <input className = "p-2 m-3 w-full h-1/3 bg-slate-100 rounded-xl " required type="email" placeholder="Email" name = "email" {...register('email')} ></input>
+      <input className = "p-2 m-3 w-full h-1/3 bg-slate-100 rounded-xl " required type = "password" placeholder="Password" name = "password" {...register('password')}></input>
+      <input className = "p-2 m-3 w-full h-1/3 bg-slate-100 rounded-xl " required type = "password" placeholder="Confirm Password" name = "passwordConfirm" {...register('passwordConfirm')}></input>
+      <input className = "p-2 m-3 w-full h-1/3 bg-slate-100 rounded-xl " required type = "text" placeholder="Farmer/Expert" name = "group" {...register('group')}></input>
       <button className=" bg-emerald-600 text-center text-white p-2 mt-4 rounded-full h-1/3  w-full active:bg-green-400" type = "Submit" disabled={isLoading}>{isLoading? "Loading":"Sign up"}</button>
     </form>
     <div className="flex flex-col w-1/4 p-6 mt-4 items-center rounded-2xl justify-center drop-shadow-2xl bg-white">
